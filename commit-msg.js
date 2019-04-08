e2bd346c1775876c0f3764fef2b24e1fa706f2d1
Luke Brobbin <lbrobbin@jaguarlandrover.com>
2019-04-08 21:56:17 +0100

    Moved funtion to correct file name

--- commit-msg.js
+++ commit-msg.js
@@ -1,53 +1,19 @@
-/*
- Copyright (C) 2018, Jaguar Land Rover - All Rights Reserved.
- CONFIDENTIAL INFORMATION - DO NOT DISTRIBUTE
-*/
+#!/usr/bin/env node
 
 const fs = require("fs");
 const path = require("path");
-const filename = process.cwd();
-const simpleGit = require("simple-git")(filename);
 
-const message = {
-  handleDiffResult(err, result, config) {
-    let overSizedFiles = 0;
-    for (const file of result.files) {
-      const stats = fs.statSync(file.file);
-      const fileSizeInBytes = stats.size;
-      const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
-      if (fileSizeInMegabytes > config.maxFileSize) {
-        console.log(`${file.file}: File size is too big`);
-        ++overSizedFiles;
-      }
-    }
-
-    if (overSizedFiles) {
-      console.log(
-        `Please un-stage ${overSizedFiles} oversized files before committing`
-      );
-      process.exit(1);
-    }
-    if (config.gitlabFileCheck) {
-      this.fileExists("/.gitlab-ci.yml");
-    }
-    if (config.esLintCheck) {
-      this.fileExists("/.eslintrc");
-    }
-  },
-  fileExists(filePath) {
-    if (fs.existsSync(path.join(process.env.PWD, filePath))) {
-      console.log(`${filePath} file exists`);
-      return true;
-    } else {
-      console.log(`${filePath} file does not exist`);
-      process.exit(1);
-    }
-  },
-  commitMessage(config) {
-    simpleGit.diffSummary(["--cached"], (err, result) => {
-      this.handleDiffResult(err, result, config);
-    });
+const getCommitMessage = () => {
+  // checks for message starting with `SPC-{JIRA_NUMBER}`
+  const COMMIT_CONTRACT = /SPC-[0-9]{1,6}/;
+  // gets the commit message from husky
+  const message = fs.readFileSync(process.env.HUSKY_GIT_PARAMS, "utf8").trim();
+  // tests message against the COMMIT_CONTRACT
+  if (!COMMIT_CONTRACT.test(message)) {
+    console.log("No Jira issue detected in commit message. Commit rejected.");
+    process.exit(1);
   }
+  console.log("Commit message has Jira issue linked.");
 };
 
-module.exports = message;
+getCommitMessage();
--- spec/pre-commit.spec.js
+++ spec/pre-commit.spec.js
@@ -1,7 +1,87 @@
 const { expect } = require("chai");
+const fs = require("fs");
+const sinon = require("sinon");
+const preCommitFunctions = require("../pre-commit");
 
 describe("pre-commit()", () => {
-  it("Should work", () => {
-    expect(true).to.be.true;
+  describe("handleDiffResult()", () => {
+    let mockResult;
+    before(() => {
+      sinon.stub(process, "exit");
+      sinon.spy(preCommitFunctions, "fileExists");
+      mockResult = {
+        files: [
+          {
+            file: "spec/pre-commit.spec.js",
+            changes: 17,
+            insertions: 15,
+            deletions: 2,
+            binary: false
+          }
+        ]
+      };
+    });
+    after(() => {
+      process.exit.restore();
+      preCommitFunctions.fileExists.restore();
+    });
+    afterEach(() => {
+      fs.statSync.restore();
+    });
+    it("Should exit if there are any oversized files", () => {
+      sinon.stub(fs, "statSync").returns({ size: 3000000 });
+      preCommitFunctions.handleDiffResult(null, mockResult, {
+        maxFileSize: 2
+      });
+      expect(process.exit.calledWith(1)).to.be.true;
+    });
+    it("If the config object contains a gitlabFileCheck property, should call fileExists with the string '/.gitlab-ci.yml'", () => {
+      sinon.stub(fs, "statSync").returns({ size: 30 });
+      preCommitFunctions.handleDiffResult(null, mockResult, {
+        maxFileSize: 2,
+        gitlabFileCheck: true
+      });
+      expect(preCommitFunctions.fileExists.calledOnceWith("/.gitlab-ci.yml")).to
+        .be.true;
+      preCommitFunctions.fileExists.resetHistory();
+    });
+    it("If the config object does not contain a gitlabFileCheck property, should not fileExists with the string '/.gitlab-ci.yml'", () => {
+      sinon.stub(fs, "statSync").returns({ size: 30 });
+      preCommitFunctions.handleDiffResult(null, mockResult, {
+        maxFileSize: 2
+      });
+      expect(preCommitFunctions.fileExists.called).to.be.false;
+    });
+    it("If the config object contains a esLintCheck property, should call fileExists with the string '/.eslintrc'", () => {
+      sinon.stub(fs, "statSync").returns({ size: 30 });
+      preCommitFunctions.handleDiffResult(null, mockResult, {
+        maxFileSize: 2,
+        esLintCheck: true
+      });
+      expect(preCommitFunctions.fileExists.calledOnceWith("/.eslintrc")).to.be
+        .true;
+      preCommitFunctions.fileExists.resetHistory();
+    });
+    it("If the config object does not contain a esLintCheck property, should not fileExists with the string '/.eslintrc'", () => {
+      sinon.stub(fs, "statSync").returns({ size: 30 });
+      preCommitFunctions.handleDiffResult(null, mockResult, {
+        maxFileSize: 2
+      });
+      expect(preCommitFunctions.fileExists.called).to.be.false;
+    });
+  });
+  describe("fileExists()", () => {
+    it("Should call process exit if the file does not exist", () => {
+      sinon.stub(process, "exit");
+      sinon.stub(fs, "existsSync").returns(false);
+      preCommitFunctions.fileExists("spec/pre-commit.spec.js");
+      expect(process.exit.calledWith(1)).to.be.true;
+      process.exit.restore();
+      fs.existsSync.restore();
+    });
+    it("Should return true if the file exists", () => {
+      expect(preCommitFunctions.fileExists("spec/pre-commit.spec.js")).to.be
+        .true;
+    });
   });
 });
